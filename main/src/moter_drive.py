200db357721b56035e65c73f6e84e55bfe94671c
obuobueven <obuobuobu10@gmail.com>
2024-10-15 15:45:38 +0800

    moter_driven

---
+++ main/src/moter_drive.py
@@ -0,0 +1,100 @@
+# 树莓派小车电机驱动程序
+
+import RPi.GPIO as gpio
+import time
+
+# 定义引脚
+pin1 = 12  # -> IN1
+pin2 = 16  # -> IN2
+pin3 = 18  # -> IN3
+pin4 = 22  # -> IN4
+ENA = 38   # -> ENA PWM
+ENB = 40   # -> ENB PWM
+
+gpio.setwarnings(False)
+# 设置gpio口为BOARD编号规范
+gpio.setmode(gpio.BOARD)
+
+# car类实现小车轮子驱动
+class Car:
+    def __init__(self, pins: list):
+        # 传入参数为一个数组，分别代表四个电机接口的GPIO口号，电机使能接口的GPIO口号
+        if len(pins) < 6:
+            raise ValueError("请提供至少6个GPIO接口的引脚号")
+        
+        self.pin1, self.pin2, self.pin3, self.pin4, self.ENA, self.ENB = pins[:6]
+
+        gpio.setup(self.pin1, gpio.OUT)
+        gpio.setup(self.pin2, gpio.OUT)
+        gpio.setup(self.pin3, gpio.OUT)
+        gpio.setup(self.pin4, gpio.OUT)
+        gpio.setup(self.ENA, gpio.OUT)
+        gpio.setup(self.ENB, gpio.OUT)
+
+
+    def forward(self):
+        gpio.output(self.pin1, gpio.HIGH)  # 将pin1接口设置为高电压
+        gpio.output(self.pin2, gpio.LOW)  # 将pin2接口设置为低电压
+        gpio.output(self.pin3, gpio.HIGH)  # 将pin3接口设置为高电压
+        gpio.output(self.pin4, gpio.LOW)  # 将pin4接口设置为低电压
+        print("forward")
+
+    def backward(self):
+        gpio.output(self.pin1, gpio.LOW)
+        gpio.output(self.pin2, gpio.HIGH)
+        gpio.output(self.pin3, gpio.LOW)
+        gpio.output(self.pin4, gpio.HIGH)
+        print("backward")
+
+    def left(self):
+        gpio.output(self.pin1, gpio.LOW)
+        gpio.output(self.pin2, gpio.HIGH)
+        gpio.output(self.pin3, gpio.HIGH)
+        gpio.output(self.pin4, gpio.LOW)
+        print("left")
+
+    def right(self):
+        gpio.output(self.pin1, gpio.HIGH)
+        gpio.output(self.pin2, gpio.LOW)
+        gpio.output(self.pin3, gpio.LOW)
+        gpio.output(self.pin4, gpio.HIGH)
+        print("right")
+
+    def stop(self):
+        gpio.output(self.pin1, gpio.LOW)
+        gpio.output(self.pin2, gpio.LOW)
+        gpio.output(self.pin3, gpio.LOW)
+        gpio.output(self.pin4, gpio.LOW)
+        print("stpo")
+
+
+def run():
+    # 实例
+    car = Car(pins=[pin1,pin2,pin3,pin4,ENA,ENB])
+    # pwm初始化
+    pwm1 = gpio.PWM(car.ENA, 50)
+    pwm2 = gpio.PWM(car.ENB, 50)
+
+    # pwm开始输出
+    pwm1.start(0)
+    pwm2.start(0)
+
+    # 前进
+    car.forward()
+    print("GO!")
+
+    # 让小车每秒钟逐渐增加速度
+    for i in range(6):
+        pwm1.ChangeDutyCycle(10 * i)
+        pwm2.ChangeDutyCycle(10 * i)
+        time.sleep(0.2)
+        print(i,"'s speed up!")
+
+    print("Over")
+    # 让小车停止,释放资源
+    gpio.cleanup()
+    pwm1.stop()
+    pwm2.stop()
+
+if __name__ == "__main__":
+    run()
